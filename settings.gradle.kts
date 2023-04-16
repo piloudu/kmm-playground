134b22a321999f2ed6e1c3515291eec12f87a06e
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-04-16 16:54:36 +0200

    REFACTOR: Use infix functions for plugin versions in the settings.gradle.kts file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,14 +14,13 @@ pluginManagement {
         val agpVersion = extra["agp.version"] as String
         val composeVersion = extra["compose.version"] as String
 
-        kotlin("jvm").version(kotlinVersion)
-        kotlin("multiplatform").version(kotlinVersion)
-        kotlin("android").version(kotlinVersion)
+        kotlin("jvm") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
+        kotlin("android") version kotlinVersion
 
-        id("com.android.application").version(agpVersion)
-        id("com.android.library").version(agpVersion)
-
-        id("org.jetbrains.compose").version(composeVersion)
+        id("com.android.application") version agpVersion
+        id("com.android.library") version agpVersion
+        id("org.jetbrains.compose") version composeVersion
     }
 }
 
