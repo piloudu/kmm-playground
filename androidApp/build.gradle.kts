6320d2ab505abfdb0cbb64861bd62bf868020c9b
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-04-16 22:32:48 +0200

    REFACTOR: Rename namespace to drop myapplication reference from the project template

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,7 +17,7 @@ kotlin {
 
 android {
     compileSdk = (findProperty("android.compileSdk") as String).toInt()
-    namespace = "com.myapplication"
+    namespace = "com.kmm.playground"
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
 
