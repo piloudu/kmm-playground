8300d3f779012bd40aa7663910a40d44adeeed3e
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-04-17 21:47:29 +0200

    ADD: Repositories for dependencies and versions catalog in the gradle-plugins module

--- gradle-plugins/settings.gradle.kts
+++ gradle-plugins/settings.gradle.kts
@@ -1,1 +1,13 @@
 include(":conventions")
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    versionCatalogs.register("libsPlugins") {
+        from(files("../gradle/libs.versions.toml"))
+    }
+}
